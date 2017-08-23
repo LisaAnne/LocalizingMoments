8923d4ecedc7e0827d2c73067adcf82f16db5e0e
lhendric <lhendric@adobe.com>
2017-08-22 23:15:37 -0700

    initial data files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # LocalizingMoments
+
+##Dataset
+
+To access the dataset, please look at the json files in the "data" folder.  The json files contain a list of data points which are stored as dict structures with the following fields:
+	annotation_id: Annotation ID for data point
+        description: Description for a specific video segment
+ 	video: Video id
+	times: Ground truth time points marked by annotators
+        download_link: a download link for the video
+        num_segments:  how many GIFs were shown to annotators.  Some videos are a little shorter than 25 seconds, so were split into five GIFs instead of six.
+
+##Downloading videos
+
+Use the script download_videos.py:
+python download_videos.py  --download --video_directory DIRECTORY
+
+There are some videos which have been removed from Flickr (~3% of the original videos).  You may access them at my website: https://people.eecs.berkeley.edu/~lisa_anne/didemo/data/missing_videos/missing_videos.zip
+
+Please contact me if you find more videos are missing.  I originally downloaded the videos provided in the YFCC100M dataset (10th entry for lines in ``video_licenses.txt''), but many of these links no longer work.  The srcript I used to download the videos should work, but let me know if you run into any issues.  If you have a better understanding of the FlickrAPI (or are familiar with YFCC100M) and know why I can no longer download videos using the original download links, let me know.
+
+You can view the Creative Commons licenses and all metadata in ``video_licenses.txt''.
+
---
+++ download_videos.py
@@ -0,0 +1,66 @@
+''' 
+Code to get the data in the DiDeMo video dataset.
+
+Usage:
+
+python download_videos.py  --download --video_directory DIRECTORY
+
+will download videos from flickr to DIRECTORY
+
+python download_videos.py  (without the download flag), will just check to see if a video is still on the flickr.  As of 08/20/2017, 316 of the origianl >10,000 videos have been removed from Flickr.  I will place these on my website shortly (as well as pre-extracted features).
+
+'''
+
+import sys
+import os
+import python_tools
+import urllib
+import urllib2
+import argparse
+import pdb
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--video_directory", type=str, default='videos/', help="Indicate where you want downloaded videos to be stored")
+parser.add_argument("--download", dest="download", action="store_true")
+parser.set_defaults(download=False)
+args = parser.parse_args()
+if args.download:
+    assert os.path.exists(args.video_directory)
+
+train_caps = python_tools.read_json('data/train_data.json')
+val_caps = python_tools.read_json('data/val_data.json')
+test_caps = python_tools.read_json('data/test_data.json')
+
+all_data = train_caps + val_caps + test_caps
+
+link_dict = {}
+for d in all_data:
+    link_dict[d['dl_link']] = d['video']
+
+count = 0
+missing_videos_list = []
+for dl_link, video_name in zip(link_dict.keys()[:50], link_dict.values()[:50]):
+    sys.stdout.write('\rDownlaoding video: %d/%d' %(count, len(link_dict.keys())))
+    if args.download:
+        try:
+            response = urllib2.urlopen(dl_link)
+            urllib.urlretrieve(dl_link, '%s/%s' %(args.video_directory, video_name))
+        except:
+            print "Could not download link: %s\n" %dl_link
+    else:
+        try:
+            response = urllib2.urlopen(dl_link)
+        except:
+            print "Could not find link: %s\n" %dl_link
+            missing_videos_list.append(video_name)
+    count += 1
+print "\n"
+print "%d videos are missing" %len(missing_videos_list)
+
+
+if len(missing_videos_list) > 0:
+    write_txt = open('missing_videos.txt', 'w')
+    for missing_video in missing_videos_list:
+        write_txt.writelines('%s\n' %missing_video)
+    write_txt.close()
+
