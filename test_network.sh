021c0655a0f687b1c61304201fb9b30776c33bba
Lisa Anne Hendricks <lhendric@adobe.com>
2017-12-15 00:36:07 +0000

    echo statements for clarity in bash script

--- test_network.sh
+++ test_network.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "RGB model on val..."
 python test_network.py --deploy_net prototxts/deploy_clip_retrieval_rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2.prototxt \
                        --snapshot_tag rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                        --visual_feature feature_process_context \
@@ -10,6 +11,7 @@ python test_network.py --deploy_net prototxts/deploy_clip_retrieval_rgb_iccv_rel
                        --test_h5 data/rgb_fc7.h5 \
                        --split val \
 
+echo "Flow model on val..."
 python test_network.py --deploy_net prototxts/deploy_clip_retrieval_flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2.prototxt \
                        --snapshot_tag flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                        --visual_feature feature_process_context \
@@ -20,11 +22,13 @@ python test_network.py --deploy_net prototxts/deploy_clip_retrieval_flow_iccv_re
                        --test_h5 data/average_global_flow.h5 \
                        --split val \
 
+echo "Fusion model on val..."
 python late_fusion.py --rgb_tag rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                       --flow_tag flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                       --split val \
                       --iter 30000 \
 
+echo "RGB model on test..."
 python test_network.py --deploy_net prototxts/deploy_clip_retrieval_rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2.prototxt \
                        --snapshot_tag rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                        --visual_feature feature_process_context \
@@ -35,6 +39,7 @@ python test_network.py --deploy_net prototxts/deploy_clip_retrieval_rgb_iccv_rel
                        --test_h5 data/average_fc7.h5 \
                        --split test \
 
+echo "Flow model on test..."
 python test_network.py --deploy_net prototxts/deploy_clip_retrieval_flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2.prototxt \
                        --snapshot_tag flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                        --visual_feature feature_process_context \
@@ -45,6 +50,7 @@ python test_network.py --deploy_net prototxts/deploy_clip_retrieval_flow_iccv_re
                        --test_h5 data/average_global_flow.h5 \
                        --split test \
 
+echo "Fusion model on test..."
 python late_fusion.py --rgb_tag rgb_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                       --flow_tag flow_iccv_release_feature_process_context_recurrent_embedding_lfTrue_dv0.3_dl0.0_nlv2_nlllstm_no_embed_edl1000-100_edv500-100_pmFalse_losstriplet_lwInter0.2 \
                       --split test \
