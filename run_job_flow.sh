618bebfe6c4b897e94770b8011d34aa45c941e62
Lisa Anne Hendricks <LisaAnne@users.noreply.github.com>
2018-05-16 19:14:48 -0700

    Update run_job_flow.sh

--- run_job_flow.sh
+++ run_job_flow.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-lw=0.2
-
-#check if files have been copied to ssd
-
-didemo_data=/mnt/ssd/tmp/lhendric/didemo_data/
-kinetics_features_ssd=/mnt/ssd/tmp/lhendric/kinetics_data
-
-mkdir -p /mnt/ssd/tmp/lhendric/kinetics_data
-mkdir -p $didemo_data 
-mkdir -p /mnt/ssd/tmp/lhendric/snapshots
-
-if [ ! -d $kinetics_features_ssd ]; then
-    cp -r /mnt/ilcompf2d0/data/didemo_lhendric/kinetics_features/ $kinetics_features_ssd 
-fi
-
-if [ ! -f '/mnt/tmp/ssd/lhendric/didemo_data/average_fc7_feats_fps.h5' ]; then
-    cp data/average_fc7_feats_fps.h5 /mnt/ssd/tmp/lhendric/didemo_data/ 
-fi
-
-
 python build_net.py --feature_process_visual feature_process_context  \
                     --loc \
                     --vision_layers 2 \
